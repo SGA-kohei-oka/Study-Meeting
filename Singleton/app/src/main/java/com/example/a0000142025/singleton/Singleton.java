98ab35a6e7fabe934059ec1c5c1573b757fd6afa
SGA-kohei-oka <Kohei.Oka@jp.sony.com>
2015-06-11 17:14:48 +0900

    Singleton updated

--- Singleton/app/src/main/java/com/example/a0000142025/singleton/Singleton.java
+++ Singleton/app/src/main/java/com/example/a0000142025/singleton/Singleton.java
@@ -6,11 +6,14 @@ import android.view.ScaleGestureDetector;
  * Created by 0000142025 on 2015/06/10.
  */
 public class Singleton {
-    private static Singleton singleton = new Singleton();
     private int testNum = 0;
 
     private Singleton() {
         System.out.println("一回きりのインスタンス生成");
+    }
+
+    private static class DelayedInitialization {
+        public static final Singleton singleton = new Singleton();
     }
 
     /**
@@ -18,7 +21,7 @@ public class Singleton {
      * @return Singletonのインスタンス
      */
     public static Singleton getInstance() {
-        return singleton;
+        return DelayedInitialization.singleton;
     }
 
     /**
